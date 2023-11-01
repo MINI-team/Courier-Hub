6185d306f627b72c43c5248fe0e74d64f056a39d
Piotr Baranowski <piotr.baranowski250@gmail.com>
2023-11-01 19:57:28 +0100

    Landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,20 @@
-import './App.css'
-import FormComponent from './InquiryForm/FormComponent'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import './App.css';
+import LandingPage from './LandigPage/LandingPage';
+import FormComponent from './InquiryForm/FormComponent';
+import LoginForm from './LoginForm/LoginComponent';
 
 function App() {
   return (
-    <div>
-      <div>
-        <h1>CourierHub</h1>
-      </div>
-      <FormComponent />
-    </div>
-  )
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/form" component={FormComponent} />
+        <Route path="/login" component={LoginForm} /> {}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/LoginForm/LoginComponent.tsx
@@ -0,0 +1,6 @@
+export default function LoginComponent() {
+  return (
+    <div>
+    </div>
+  );
+}
