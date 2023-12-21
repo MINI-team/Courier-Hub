fbfff17576b9c7fbbe076d9ff54bb0b31ad9f2c9
glowacz <glowacki.pj@gmail.com>
2023-12-21 12:50:50 +0100

    get status

---
+++ DHL/API/Controllers/GetOrderStatusController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Orders;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("Order/GetStatus")]
+    // [Route("api/[controller]")]
+    public class GetOrderStatusController : BaseApiController
+    {
+        [HttpGet("{id}")]
+        public async Task<ActionResult<int>> GetStatus(int id)
+        {
+            return await Mediator.Send(new Status.Query{OrderId = id});
+        }
+    }
+}
